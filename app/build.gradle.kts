2663f99480ff1889150340b2c4966de478b4e419
Hui-Hong You <hiroshi@ghostsinthelab.org>
2025-06-02 22:20:45 +0800

    Release 0.9.1.6
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 }
 
 val projectName: String = "libchewing_android_app_module"
-val versionName: String = "0.9.1.5"
+val versionName: String = "0.9.1.6"
 
 android {
     namespace = "com.miyabi_hiroshi.app.${projectName}"
